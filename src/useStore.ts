ee265bc610e4c9f3a93f3260cf80f29cbab07252
axtk <axtkx@inbox.ru>
2025-03-23 11:56:21 +0300

    rename shouldSubscribe to isResponsive

--- src/useStore.ts
+++ src/useStore.ts
@@ -2,11 +2,11 @@ import {useEffect, useMemo, useState} from 'react';
 import {Store} from './Store';
 
 export type SetStoreState<T> = Store<T>['setState'];
-export type ShouldSubscribe<T> = (nextState: T, prevState: T) => boolean;
+export type IsResponsive<T> = (nextState: T, prevState: T) => boolean;
 
 export function useStore<T>(
     store: Store<T>,
-    shouldSubscribe: boolean | ShouldSubscribe<T> = true,
+    isResponsive: boolean | IsResponsive<T> = true,
 ): [T, SetStoreState<T>] {
     if (!(store instanceof Store))
         throw new Error('\'store\' is not an instance of Store');
@@ -17,14 +17,14 @@ export function useStore<T>(
     let setState = useMemo(() => store.setState.bind(store), [store]);
 
     useEffect(() => {
-        if (!shouldSubscribe)
+        if (!isResponsive)
             return;
 
         return store.subscribe((nextState, prevState) => {
-            if (typeof shouldSubscribe !== 'function' || shouldSubscribe(nextState, prevState))
+            if (typeof isResponsive !== 'function' || isResponsive(nextState, prevState))
                 setRevision(Math.random());
         });
-    }, [store, shouldSubscribe]);
+    }, [store, isResponsive]);
 
     return [state, setState];
 }
