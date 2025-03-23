d950d44a20b2fb26d495ab0ba7fb9d30844ea6a6
axtk <axtkx@inbox.ru>
2025-03-23 12:17:05 +0300

    add tsdoc to isResponsive option

--- src/useStore.ts
+++ src/useStore.ts
@@ -6,6 +6,23 @@ export type IsResponsive<T> = (nextState: T, prevState: T) => boolean;
 
 export function useStore<T>(
     store: Store<T>,
+    /**
+     * Controls whether the component should be updated in response
+     * to the store updates.
+     *
+     * @defaultValue `true`
+     *
+     * Can be set to `false` when the component only requires the
+     * store state setter but not the state value itself, and so the
+     * component doesn't need to respond to updates in the store state.
+     *
+     * ```ts
+     * let [, setValue] = useStore(store, false);
+     * ```
+     *
+     * Can be set to a function `(nextState, prevState) => boolean` to
+     * make the component respond only to specific store state changes.
+     */
     isResponsive: boolean | IsResponsive<T> = true,
 ): [T, SetStoreState<T>] {
     if (!(store instanceof Store))
